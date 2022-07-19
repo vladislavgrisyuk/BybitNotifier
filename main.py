1a7cfe88f5afe7ea016b4d752b975f094c9cf5e0
Vladislav Hrysiuk <vladislavgrisyuk@gmail.com>
2022-07-19 13:37:37 +0200

    Bybit change

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ from timeGenerator import generate_with_random_time
 from menu_keyboard import kb
 import logging
 import sys
+import requests
+import json
+from datetime import datetime
 
 
 logging.basicConfig(
@@ -36,6 +39,7 @@ token = os.getenv('TOKEN')
 
 bot = Bot(token)
 dp = Dispatcher(bot, storage=storage)
+listOrders = []
 
 
 class UserState(StatesGroup):
@@ -46,71 +50,33 @@ class UserState(StatesGroup):
 
 @dp.message_handler(commands=['start', 'menu'])
 async def begin(message: types.Message, state: FSMContext):
-    await message.answer('Menu', reply_markup=kb)
+    first = True
+    while(True):
+        lm1 = 'IT3R2gEhFD0qI%2FL7r3FS5g%3D%3D'
+        lm2 = 'bTTxdRatXQ6XkYDX8mHgyw=='
+        url = 'https://api2.bybit.com/fapi/beehive/public/v1/common/order/list-detail?timeStamp=1658218420604&leaderMark='
+
+        r = requests.get(url + lm1)
+
+        data = json.loads(r.text)
+
+        await bot.send_message(message.chat.id, bbt_data(data, first))
+
+        r = requests.get(url + lm2)
+
+        data = json.loads(r.text)
+
+        await bot.send_message(message.chat.id, bbt_data(data, first))
+        first = False
 
 
-@dp.message_handler(commands=['vlad'])
-@dp.message_handler(text=['Влад тест'])
-async def begin(message: types.Message, state: FSMContext):
-    log_text = '[requested file]' + \
-        '[id: ' + str(message.from_user.id) + ']' + \
-        '[full name: ' + message.from_user.full_name + ']' + \
-        '[text: ' + message.text + ']'
+def bbt_data(data, first):
+    for i in data['result']['data']:
+        if(not listOrders.__contains__(i['createdAtE3']) or first):
+            listOrders.append(i['createdAtE3'])
+            size = len(i['createdAtE3'])
 
-    logging.info(log_text)
-    pdfhelper.go('Resources/v.pdf', get_replacements())
-    await bot.send_document(message.chat.id, document=open('Resources/v.result.pdf', 'rb'))
-    os.remove('Resources/v.result.pdf')
-
-
-@dp.message_handler(commands=['nastya'])
-@dp.message_handler(text=['Настя тест'])
-async def begin(message: types.Message):
-    log_text = '[requested file]' + \
-        '[id: ' + str(message.from_user.id) + ']' + \
-        '[full name: ' + message.from_user.full_name + ']' + \
-        '[text: ' + message.text + ']'
-    logging.info(log_text)
-    r = get_replacements()
-    pdfhelper.go('Resources/n.pdf', r)
-    await bot.send_document(message.chat.id, document=open('Resources/n.result.pdf', 'rb'))
-    os.remove('Resources/n.result.pdf')
-
-
-@dp.message_handler(commands=['all'])
-@dp.message_handler(text=['Оба теста'])
-async def begin(message: types.Message):
-    log_text = '[requested files]' + \
-        '[id: ' + str(message.from_user.id) + ']' + \
-        '[full name: ' + message.from_user.full_name + ']' + \
-        '[text: ' + message.text + ']'
-
-    logging.info(log_text)
-    r_small = randint(2, 5)
-    time_gen = generate_with_random_time(9, 13)
-    r_time_hours = time_gen.hour
-    r_time_minutes = time_gen.minute
-
-    time = str(r_time_hours).zfill(2) + ':' + str(r_time_minutes).zfill(2)
-
-    time_gen_delta = time_gen + datetime.timedelta(minutes=15+r_small)
-    time_minutes_delta = r_time_minutes + 15
-    time_delta = str(r_time_hours).zfill(2) + ':' + \
-        str(time_minutes_delta).zfill(2)
-
-    r = get_replacements(r_time_hours, r_time_minutes)
-    r2 = get_replacement_with_time(time_gen, time_gen_delta)
-    pdfhelper.go('Resources/n.pdf', r2)
-    await bot.send_document(message.chat.id, document=open('Resources/n.result.pdf', 'rb'))
-
-    r = get_replacements(r_time_hours, r_time_minutes + r_small)
-    r2 = get_replacement_with_time(time_gen + datetime.timedelta(
-        minutes=r_small), time_gen_delta + datetime.timedelta(minutes=r_small))
-    pdfhelper.go('Resources/v.pdf', r2)
-    await bot.send_document(message.chat.id, document=open('Resources/v.result.pdf', 'rb'))
-
-    os.remove('Resources/n.result.pdf')
-    os.remove('Resources/v.result.pdf')
+            return 'Монета: ' + i['symbol'] + '\n' + 'Вид: ' + i['side'] + '\n' + 'Курс входа: ' + i['entryPrice'] + '$' + '\n' + 'Время входа: ' + str(datetime.fromtimestamp(int(i['createdAtE3'][:size - 3]))) + '\n' + 'Маржа: ' + i['leverageE2']
 
 
 async def on_startup(dispatcher):
